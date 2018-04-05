6791290ea628c04b26a5e7257808898b48bedefb
Jacob Koperski <wijakoperski@gmail.com>
2018-04-05 10:31:01 -0500

    Fixed broken unit test

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -81,7 +81,7 @@ namespace TravisCILab
         [Test]
         public void Divide_Valid()
         {
-            Assert.AreEqual(5, Program.Divide("1", "2"));
+            Assert.AreEqual(0.5, Program.Divide("1", "2"));
             Assert.AreEqual(1.5, Program.Divide("3", "2"));
             Assert.AreEqual(0.7142857142857143, Program.Divide("5", "7"));
         }
